93b380dc3e6223d51b2edfe8dcc66e6534e2e4c9
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-25 18:03:22 +0100

    .

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Usa una imagen base de Java 17
+FROM openjdk:17-jdk-slim
+
+# Establece el directorio de trabajo
+WORKDIR /app
+
+# Copia el archivo JAR al contenedor
+COPY target/Microservicio1-0.0.1-SNAPSHOT.jar /app/microservicio1.jar
+
+# Define el puerto en el que se ejecutará la aplicación
+EXPOSE 8080
+
+# Comando para ejecutar la aplicación
+ENTRYPOINT ["java", "-jar", "/app/microservicio1.jar"]
--- src/main/java/org/example/microservicio1/Microservicio1Application.java
+++ src/main/java/org/example/microservicio1/Microservicio1Application.java
@@ -3,6 +3,7 @@ package org.example.microservicio1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Microservicio1Application {
@@ -10,5 +11,4 @@ public class Microservicio1Application {
     public static void main(String[] args) {
         SpringApplication.run(Microservicio1Application.class, args);
     }
-
 }
